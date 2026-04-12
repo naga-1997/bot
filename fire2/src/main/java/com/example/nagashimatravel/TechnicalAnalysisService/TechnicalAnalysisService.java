7d219efe832a6c94193175531db17fbc901bbee1
Kazuki Nagashima <kazumaru.1925@gmail.com>
2026-04-12 10:35:13 +0900

    エラー解消済み

--- fire2/src/main/java/com/example/nagashimatravel/TechnicalAnalysisService/TechnicalAnalysisService.java
+++ fire2/src/main/java/com/example/nagashimatravel/TechnicalAnalysisService/TechnicalAnalysisService.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.stereotype.Service;
 
+import com.example.nagashimatravel.SignalCandle.SignalCandle;
+
 @Service
 public class TechnicalAnalysisService {
 
