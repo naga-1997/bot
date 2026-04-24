ea06ed102778c1a5334a5227df5c996f453d380d
naga-1997 <bb4.444.kazu@gmail.com>
2026-04-24 16:27:00 +0900

    test

--- fire2/src/main/java/com/example/nagashimatravel/Fire2Application.java
+++ fire2/src/main/java/com/example/nagashimatravel/Fire2Application.java
@@ -7,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class Fire2Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Fire2Application.class, args);
 	}
